3b567787d46c7d2734658b50df05c01b4fc838e4
Vaishnavnandre3040 <vaishnavnandre3040@gmail.com>
2025-03-14 19:15:42 +0530

    Update HelloWorld.java 3

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Installed git");
+        System.out.println("Now retrying");
         System.out.println("Task done!");  
     }
 }
