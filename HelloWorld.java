b3a1bdaf6fc0b9c1fbd7dcc82aaabd143e8d9eb8
Vaishnavnandre3040 <vaishnavnandre3040@gmail.com>
2025-10-01 01:44:53 +0530

    Update HelloWorld.java added webhook line

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Installed git");
-        System.out.println("Now retrying");
+        System.out.println("Now trying to use webhook");
         System.out.println("Task done!"); 
         System.out.println("---Thank you------!"); 
         System.out.println("-------------------!");
