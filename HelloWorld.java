6b18c10a66554568ed87e5eac4b8626cdee4566d
Vaishnavnandre3040 <vaishnavnandre3040@gmail.com>
2025-10-01 11:51:43 +0530

    added date

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
         System.out.println("Hello, World!");
         System.out.println("Installed git");
         System.out.println("Now trying to use webhook");
-        System.out.println("Task done!"); 
+        System.out.println("Task done! 01/10/25"); 
         System.out.println("---Thank you------!"); 
         System.out.println("-------------------!");
         System.out.println("-________******_______!");        
